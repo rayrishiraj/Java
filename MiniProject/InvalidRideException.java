bd37b2b36c1510a1730ad3a8f41912c0b6621d98
Rishiraj Ray <136910700+rayrishiraj@users.noreply.github.com>
2025-04-28 13:46:04 +0530

    Delete MiniProject directory

--- MiniProject/InvalidRideException.java
+++
@@ -1,9 +0,0 @@
-package MiniProject;
-
-@SuppressWarnings("serial")
-public class InvalidRideException extends RuntimeException {
-
-	public InvalidRideException(String message) {
-		super(message);
-	}
-}
